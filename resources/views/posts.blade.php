ac2bf88fb83cc03cdb043ec054a92715e8fa2051
fahmihwan <fahmiiwan86@gmail.com>
2022-03-01 03:11:09 +0700

    N+1 Problem

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,13 @@
 @extends('layout/layout')
 
 @section('container')
-<h1>Halaman Blog Posts</h1>
+<h1>{{ $title  }}</h1>
 
 @foreach($posts as $post)
 <article class="mb-5  border-bottom pb-4">
     <h5>
         <a href="/post/{{$post->slug}}" class="text-decoration-none">
-            {{ $post->title }} <!-- lorem ipsum pertama -->
+            {{ $post->title }}
         </a>
     </h5>
     <p>By. <a href="/authors/{{ $post->author->username }}">{{ $post->author->name  }}</a> in <a href="/categories/{{$post->category->slug}}" class="text-decoration-none">{{$post->category->name}}</a></p>
